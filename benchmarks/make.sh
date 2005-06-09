93bdbe039768a7455250496c3ea0289edaa234a6
Jérôme Euzenat <Jerome.Euzenat@inria.fr>
2005-06-09 14:15:04 +0000

    - finished complete generation script

--- benchmarks/make.sh
+++ benchmarks/make.sh
@@ -1,5 +1,5 @@
 #!/bin/csh
-# $Id$
+# $Id: make.sh,v 1.1 2005/06/08 16:42:54 euzenat Exp euzenat $
 # XSLT based test generation.
 # //pass1: generate test files
 # //pass2: fix URI
@@ -18,6 +18,7 @@ echo "================"
 /bin/rm -rf 101
 mkdir 101
 cp onto.rdf 101/
+chmod 664 101/onto.rdf
 cp refalign.rdf 101/
 
 #####################################################################
@@ -59,12 +60,13 @@ mkdir 202
 xsltproc xslt/strip-comments.xsl 201/onto.rdf > 202/onto.rdf
 xsltproc xslt/trans-random.xsl refalign.rdf > 202/refalign.rdf
 
+#####################################################################
 # 203) Systematic: No comment (was Misspelling)
 
 #DONE!
 \rm -rf 203
 mkdir 203
-xsltproc xslt/trans-comments.xsl onto.rdf > 203/onto.rdf
+xsltproc xslt/strip-comments.xsl onto.rdf > 203/onto.rdf
 cp refalign.rdf 203/refalign.rdf
 
 #####################################################################
@@ -150,12 +152,21 @@ cp refalign.rdf 221/refalign.rdf
 #A hierarchy still exists but has been strictly reduced.
 
 #//TO BE UPDATED
+# In the flattened case, some classes must be suppressed from the hierarchy
+\rm -rf 222
+mkdir 222
+cp 101/onto.rdf 222/onto.rdf
+cp refalign.rdf 222/refalign.rdf
 
 #####################################################################
 # 223) Systematic: Expanded hierarchy
 #Numerous intermediate classes are introduced within the hierarchy.
 
 #//TO BE UPDATED
+\rm -rf 223
+mkdir 223
+cp 101/onto.rdf 223/onto.rdf
+cp refalign.rdf 223/refalign.rdf
 
 #####################################################################
 # 224) Systematic: No instances
@@ -189,10 +200,11 @@ cp refalign.rdf 225/refalign.rdf
 #Properties and relations between objects have been completely suppressed.
 #(variation: leave the properties in instances)
 
-#//MISS ALIGNMENT MODIFICATION
+#DONE!
 \rm -rf 228
 mkdir 228
 xsltproc xslt/strip-properties.xsl onto.rdf > 228/onto.rdf
+xsltproc xslt/strip-propalign.xsl refalign.rdf > 228/refalign.rdf
 
 # 229) Systematic: Class vs instances
 #NOTTODO
@@ -203,12 +215,20 @@ xsltproc xslt/strip-properties.xsl onto.rdf > 228/onto.rdf
 #Here one limitation of the proposed format is that it does not cover  alignments such as: journalName = name o journal.
 
 #//TO BE UPDATED
+# when flattening not all alignment should be preserved
+\rm -rf 230
+mkdir 230
+cp 101/onto.rdf 230/onto.rdf
+cp refalign.rdf 230/refalign.rdf
 
+#//TODO
 # 231) Systematic: Multiplying entities
 #Some classes are spreaded over several classes.
 #Not available in this test.
-
-#NOTTODO
+\rm -rf 231
+mkdir 231
+cp 101/onto.rdf 231/onto.rdf
+cp refalign.rdf 231/refalign.rdf
 
 #####################################################################
 # 232) Systematic: No instances, No hierarchy
@@ -222,12 +242,140 @@ cp 221/refalign.rdf 232/refalign.rdf
 #####################################################################
 # 233) Systematic: No property, No hierarchy
 
-# //MISSING STUFF
+# DONE!
 \rm -rf 233
 mkdir 233
 xsltproc xslt/strip-properties.xsl 221/onto.rdf > 233/onto.rdf
+xsltproc xslt/strip-propalign.xsl 221/refalign.rdf > 233/refalign.rdf
 
 #####################################################################
+# 234) Systematic: No hierarchy, flattened classes
+
+# DONE!
+\rm -rf 234
+mkdir 234
+xsltproc xslt/strip-subclass.xsl 230/onto.rdf > 234/onto.rdf
+cp 230/refalign.rdf 234/refalign.rdf
+
+#####################################################################
+# 235) Systematic: No hierarchy, expansed classes
+
+# DONE!
+\rm -rf 235
+mkdir 235
+xsltproc xslt/strip-subclass.xsl 231/onto.rdf > 235/onto.rdf
+cp 231/refalign.rdf 235/refalign.rdf
+
+######################################################################
+# 236) Systematic: No instances, No properties
+
+#DONE!
+\rm -rf 236
+mkdir 236
+xsltproc xslt/strip-properties.xsl 224/onto.rdf > 236/onto.rdf
+xsltproc xslt/strip-propalign.xsl 224/refalign.rdf > 236/refalign.rdf
+
+#####################################################################
+# 237) Systematic: No instances, flattened classes
+
+# DONE!
+\rm -rf 237
+mkdir 237
+xsltproc xslt/strip-instances.xsl 230/onto.rdf > 237/onto.rdf
+cp 230/refalign.rdf 237/refalign.rdf
+
+#####################################################################
+# 238) Systematic: No instances, expansed classes
+
+# DONE!
+\rm -rf 238
+mkdir 238
+xsltproc xslt/strip-instances.xsl 231/onto.rdf > 238/onto.rdf
+cp 231/refalign.rdf 238/refalign.rdf
+
+#####################################################################
+# 239) Systematic: No properties, flattened classes
+
+#DONE!
+\rm -rf 239
+mkdir 239
+xsltproc xslt/strip-properties.xsl 230/onto.rdf > 239/onto.rdf
+xsltproc xslt/strip-propalign.xsl 230/refalign.rdf > 239/refalign.rdf
+
+#####################################################################
+# 240) Systematic: No properties, expansed classes
+
+#DONE!
+\rm -rf 240
+mkdir 240
+xsltproc xslt/strip-properties.xsl 231/onto.rdf > 240/onto.rdf
+xsltproc xslt/strip-propalign.xsl 231/refalign.rdf > 240/refalign.rdf
+
+######################################################################
+# 241) Systematic: No instances, No hierarchy, No properties
+
+# DONE!
+\rm -rf 241
+mkdir 241
+xsltproc xslt/strip-instances.xsl 233/onto.rdf > 241/onto.rdf
+cp 233/refalign.rdf 241/refalign.rdf
+
+#####################################################################
+# 242) Systematic: No instances, No hierarchy, flattened classes
+
+# DONE!
+\rm -rf 242
+mkdir 242
+xsltproc xslt/strip-instances.xsl 234/onto.rdf > 242/onto.rdf
+cp 234/refalign.rdf 242/refalign.rdf
+
+#####################################################################
+# 243) Systematic: No instances, No hierarchy, expansed classes
+
+# DONE!
+\rm -rf 243
+mkdir 243
+xsltproc xslt/strip-instances.xsl 235/onto.rdf > 243/onto.rdf
+cp 235/refalign.rdf 243/refalign.rdf
+
+######################################################################
+# 244) Systematic: No properties, No hierarchy, flattened classes
+
+#DONE!
+\rm -rf 244
+mkdir 244
+xsltproc xslt/strip-properties.xsl 234/onto.rdf > 244/onto.rdf
+xsltproc xslt/strip-propalign.xsl 234/refalign.rdf > 244/refalign.rdf
+
+
+#####################################################################
+# 245) Systematic: No properties, No hierarchy, expansed classes
+
+#DONE!
+\rm -rf 245
+mkdir 245
+xsltproc xslt/strip-properties.xsl 235/onto.rdf > 245/onto.rdf
+xsltproc xslt/strip-propalign.xsl 235/refalign.rdf > 245/refalign.rdf
+
+######################################################################
+# 246) Systematic: No properties, No instances, flattened classes
+
+#DONE!
+\rm -rf 246
+mkdir 246
+xsltproc xslt/strip-properties.xsl 237/onto.rdf > 246/onto.rdf
+xsltproc xslt/strip-propalign.xsl 237/refalign.rdf > 246/refalign.rdf
+
+#####################################################################
+# 247) Systematic: No properties, No instances, expansed classes
+
+#DONE!
+\rm -rf 247
+mkdir 247
+xsltproc xslt/strip-properties.xsl 238/onto.rdf > 247/onto.rdf
+xsltproc xslt/strip-propalign.xsl 238/refalign.rdf > 247/refalign.rdf
+
+####################################################################
 # 248) Systematic: No names, no hierarchy
 
 # DONE
@@ -246,6 +394,179 @@ xsltproc xslt/strip-instances.xsl 202/onto.rdf > 249/onto.rdf
 cp 202/refalign.rdf 249/refalign.rdf
 
 #####################################################################
+# 250) Systematic: No names, no properties
+
+#DONE!
+\rm -rf 250
+mkdir 250
+xsltproc xslt/strip-properties.xsl 202/onto.rdf > 250/onto.rdf
+xsltproc xslt/strip-propalign.xsl 202/refalign.rdf > 250/refalign.rdf
+
+#####################################################################
+# 251) Systematic: No names, flattened classes
+
+#DONE
+\rm -rf 251
+mkdir 251
+xsltproc xslt/strip-comments.xsl 230/onto.rdf > 251/preonto.rdf
+xsltproc xslt/trans-random.xsl 251/preonto.rdf > 251/onto.rdf
+xsltproc xslt/trans-random.xsl 230/refalign.rdf > 251/refalign.rdf
+
+#####################################################################
+# 252) Systematic: No names, expanded classes
+
+#DONE
+\rm -rf 252
+mkdir 252
+xsltproc xslt/strip-comments.xsl 231/onto.rdf > 252/preonto.rdf
+xsltproc xslt/trans-random.xsl 252/preonto.rdf > 252/onto.rdf
+xsltproc xslt/trans-random.xsl 231/refalign.rdf > 252/refalign.rdf
+
+#####################################################################
+# 253) Systematic: No names, no hierarchy, no instance
+
+#DONE!
+\rm -rf 253
+mkdir 253
+xsltproc xslt/strip-instances.xsl 248/onto.rdf > 253/onto.rdf
+cp 248/refalign.rdf 253/refalign.rdf
+
+#####################################################################
+# 254) Systematic: No names, no hierarchy, no properties
+
+#DONE
+\rm -rf 254
+mkdir 254
+xsltproc xslt/strip-properties.xsl 248/onto.rdf > 254/onto.rdf
+xsltproc xslt/strip-propalign.xsl 248/refalign.rdf > 254/refalign.rdf
+
+#####################################################################
+# 255) Systematic: No names, No hierarchy, flattened classes
+
+#DONE
+\rm -rf 255
+mkdir 255
+xsltproc xslt/strip-subclass.xsl 251/onto.rdf > 255/onto.rdf
+cp 251/refalign.rdf 255/refalign.rdf
+
+#####################################################################
+# 256) Systematic: No names, No hierarchy, expanded classes
+
+#DONE
+\rm -rf 256
+mkdir 256
+xsltproc xslt/strip-subclass.xsl 252/onto.rdf > 256/onto.rdf
+cp 252/refalign.rdf 256/refalign.rdf
+
+#####################################################################
+# 257) Systematic: No names, no instances, no properties
+
+#DONE
+\rm -rf 257
+mkdir 257
+xsltproc xslt/strip-properties.xsl 249/onto.rdf > 257/onto.rdf
+xsltproc xslt/strip-propalign.xsl 249/refalign.rdf > 257/refalign.rdf
+
+#####################################################################
+# 258) Systematic: No names, No instances, flattened classes
+
+#DONE
+\rm -rf 258
+mkdir 258
+xsltproc xslt/strip-instances.xsl 251/onto.rdf > 258/onto.rdf
+cp 251/refalign.rdf 258/refalign.rdf
+
+#####################################################################
+# 259) Systematic: No names, No instances, expanded classes
+
+#DONE
+\rm -rf 259
+mkdir 259
+xsltproc xslt/strip-instances.xsl 252/onto.rdf > 259/onto.rdf
+cp 252/refalign.rdf 259/refalign.rdf
+
+#####################################################################
+# 260) Systematic: No names, No properties, flattened classes
+
+#DONE
+\rm -rf 260
+mkdir 260
+xsltproc xslt/strip-properties.xsl 251/onto.rdf > 260/onto.rdf
+xsltproc xslt/strip-propalign.xsl 251/refalign.rdf > 260/refalign.rdf
+
+#####################################################################
+# 261) Systematic: No names, No properties, expanded classes
+
+#DONE
+\rm -rf 261
+mkdir 261
+xsltproc xslt/strip-properties.xsl 252/onto.rdf > 261/onto.rdf
+xsltproc xslt/strip-propalign.xsl 252/refalign.rdf > 261/refalign.rdf
+
+#####################################################################
+# 262) Systematic: No names, no instances, no properties
+
+#DONE
+\rm -rf 262
+mkdir 262
+xsltproc xslt/strip-properties.xsl 253/onto.rdf > 262/onto.rdf
+xsltproc xslt/strip-propalign.xsl 253/refalign.rdf > 262/refalign.rdf
+
+#####################################################################
+# 263) Systematic: No names, No instances, No hierarchy, flattened classes
+
+#DONE
+\rm -rf 263
+mkdir 263
+xsltproc xslt/strip-instances.xsl 255/onto.rdf > 263/onto.rdf
+cp 255/refalign.rdf 263/refalign.rdf
+
+#####################################################################
+# 264) Systematic: No names, No instances, No hierarchy, expanded classes
+
+#DONE
+\rm -rf 264
+mkdir 264
+xsltproc xslt/strip-instances.xsl 256/onto.rdf > 264/onto.rdf
+cp 256/refalign.rdf 264/refalign.rdf
+
+#####################################################################
+# 265) Systematic: No names, No instances, No hierarchy, flattened classes
+
+#DONE
+\rm -rf 265
+mkdir 265
+xsltproc xslt/strip-instances.xsl 260/onto.rdf > 265/onto.rdf
+cp 260/refalign.rdf 265/refalign.rdf
+
+#####################################################################
+# 266) Systematic: No names, No instances, No hierarchy, expanded classes
+
+#DONE
+\rm -rf 266
+mkdir 266
+xsltproc xslt/strip-instances.xsl 261/onto.rdf > 266/onto.rdf
+cp 261/refalign.rdf 266/refalign.rdf
+
+#####################################################################
+# 267) Systematic: Nothing, flattened classes
+
+#DONE
+\rm -rf 267
+mkdir 267
+xsltproc xslt/strip-subclass.xsl 265/onto.rdf > 267/onto.rdf
+cp 265/refalign.rdf 267/refalign.rdf
+
+#####################################################################
+# 268) Systematic: Nothing, expanded classes
+
+#DONE
+\rm -rf 268
+mkdir 268
+xsltproc xslt/strip-subclass.xsl 266/onto.rdf > 268/onto.rdf
+cp 266/refalign.rdf 268/refalign.rdf
+
+#####################################################################
 # //pass2: fix URI
 # DONE!
 
@@ -253,24 +574,24 @@ echo
 echo Fixing URI
 echo "=========="
 
-#for i in `ls -d [0-9][0-9][0-9]` 
-#do
-#echo "**" $i "**"
-#if [ -f $i/refalign.rdf ]
-#then
-#ed $i/refalign.rdf << EOF
-#1,$ s;entity2 rdf:resource="http://oaei.inrialpes.fr/2005/benchmark/101/;entity2 rdf:resource="http://oaei.inrialpes.fr/2005/benchmark/$i/;
-#w
-#EOF
-#fi
-#if [ -f $i/onto.rdf ]
-#then
-#ed $i/onto.rdf << EOF
-#1,$ s;oaei.inrialpes.fr/2005/benchmark/101/;oaei.inrialpes.fr/2005/benchmark/$i/;
-#w
-#EOF
-#fi
-#done
+for i in `ls -d [0-9][0-9][0-9]` 
+do
+echo -n "*"$i"*"
+if [ -f $i/refalign.rdf ]
+then
+ed $i/refalign.rdf << EOF
+1,$ s;entity2 rdf:resource="http://oaei.inrialpes.fr/2005/benchmark/101/;entity2 rdf:resource="http://oaei.inrialpes.fr/2005/benchmark/$i/;
+w
+EOF
+fi
+if [ -f $i/onto.rdf ]
+then
+ed $i/onto.rdf << EOF
+1,$ s;oaei.inrialpes.fr/2005/benchmark/101/;oaei.inrialpes.fr/2005/benchmark/$i/;
+w
+EOF
+fi
+done
 
 #####################################################################
 # //pass3: generate HTML
@@ -280,10 +601,10 @@ echo
 echo Generating HTML
 echo "==============="
 
-#for i in `ls -d [0-9][0-9][0-9]` 
-#do
-#echo "**" $i "**"
-#xsltproc xslt/form-align.xsl $i/refalign.rdf > $i/refalign.html
-#xsltproc xslt/owl2html.xsl $i/onto.rdf > $i/onto.html
-#done
+for i in `ls -d [0-9][0-9][0-9]` 
+do
+echo -n "*"$i"*"
+xsltproc xslt/form-align.xsl $i/refalign.rdf > $i/refalign.html
+xsltproc xslt/owl2html.xsl $i/onto.rdf > $i/onto.html
+done
 
